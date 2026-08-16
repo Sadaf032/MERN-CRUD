536e51d9a0c447f2eb42aacfbbc85fa27bb6f6c2
Sadaf Kareem <sadafkareem234@gmail.com>
2026-08-16 14:36:03 +0500

    Fix port for deployment

--- server/index.js
+++ server/index.js
@@ -132,6 +132,8 @@ app.put("/users/:id", async (req, res) => {
   }
 });
 
-app.listen(5000, () => {
-  console.log("Server is running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
